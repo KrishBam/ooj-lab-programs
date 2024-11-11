06f8899d93221663ce0ecb0abc7938dd944a7a0c
KrishBam <krishbam.cs23@bmsce.ac.in>
2024-11-11 16:03:21 +0530

    Add files via upload

---
+++ Externals.java
@@ -0,0 +1,12 @@
+package SEE;
+import CIE.Students;
+
+public class Externals extends Students{
+    public int external_marks[];
+    
+    public Externals(String name, String usn, int sem, int external_marks[]){
+        super(name,usn,sem);
+        this.external_marks = external_marks;
+    }
+}
+
